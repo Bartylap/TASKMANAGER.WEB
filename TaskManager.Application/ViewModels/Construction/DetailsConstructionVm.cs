019df7e790f44557065ccd8926eb37887770b322
bartylap <bartyla.piotr@gmail.com>
2021-08-18 09:39:55 +0200

    sr
    sr

--- TaskManager.Application/ViewModels/Construction/DetailsConstructionVm.cs
+++ TaskManager.Application/ViewModels/Construction/DetailsConstructionVm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using TaskManager.Application.Mapping;
+using TaskManager.Domain.Models;
 
 namespace TaskManager.Application.ViewModels.Construction
 {
@@ -16,22 +17,33 @@ namespace TaskManager.Application.ViewModels.Construction
         public string City { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+        public bool Bufor { get; set; }
 
-        public List<EmployeeListAtTheConstructionVm> Employees {get;set;}
         public List<CarsListAtTheConstructionVm> Car { get; set; }
         public List<FlatListAtTheConstructionVm> Flat { get; set; }
         public List<ToolListAtTheConstructionVm> Tolls { get; set; }
-        public bool Bufor { get; set; }
+
+        public List<EmployeeListAtTheConstructionVm> Employees { get; set; }
 
 
         public void Mapping(Profile profile)
         {
 
             profile.CreateMap<DetailsConstructionVm, TaskManager.Domain.Models.Employee>()
-                .ForMember(src => src.FirstName, opt => opt.MapFrom(dest => dest.Employees))
-                .ForMember(src => src.LastName, opt => opt.MapFrom(dest => dest.Employees[));
+                .ForMember(src => src.FirstName, opt => opt.MapFrom(dest => dest.Employees[0].FirstName))
+                .ForMember(src => src.LastName, opt => opt.MapFrom(dest => dest.Employees[0].LastName))
+                .ForMember(src => src.ConstructionEmployees, opt => opt.MapFrom(dest => dest.Employees))
+                .AfterMap((src, dest) =>
+                {
+                    foreach (var b in dest.ConstructionEmployees)
+                    {
+                        b.ConstructionId = dest.Id;
+                    }
+                });
 
-
+            profile.CreateMap<TaskManager.Domain.Models.Construction, ConstructionEmployee>()
+                .ForMember(dest => dest.ConstructionId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Construction, opt => opt.MapFrom(src => src));
 
             profile.CreateMap<TaskManager.Domain.Models.Construction, DetailsConstructionVm>().ReverseMap()
                  .ForMember(a => a.Car, opt => opt.MapFrom(b => b.Car))
